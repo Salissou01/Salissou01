9d19d9dfc98bfd12e105e7846322b5c364b70820
Salissou01 <issoufou365@gmail.com>
2022-03-08 13:44:13 +0100

    debut du project

---
+++ asm/resources/views/user.blade.php
@@ -0,0 +1,16 @@
+@extends('global.index')
+
+@section('content')
+<div class="front">
+
+                                    
+    <div class="user">
+        <img class="img-circle" src="http://profileme.originlabsoft.com/assets/front/images/avatar.png">
+</div>
+         <div>                               
+        <a href="#" class="btn default mr-5"><i class="fa fa-user-plus"></i> Follow</a>
+
+        <a target="_blank" href="#" class="btn default"><i class="fa fa-eye"></i> View Profile</a>
+    </div>
+</div>
+@endsection
---
+++ asm/routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\usecontroller;
+use App\Http\Controllers\Auth\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('accueill');
+});
+
+Auth::routes();
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('user',[usecontroller::class,'index'])->name('voir');
